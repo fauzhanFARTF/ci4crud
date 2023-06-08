47d2c2346a88193d824474f61be4dd82e79f1126
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-08 19:53:06 +0700

    Create Models ModelPegawai.php

---
+++ app/Models/ModelPegawai.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelPegawai extends Model
+{
+    protected $table = "pegawai";
+    protected $primaryKey = "id" ;
+    protected $allowedFields = ['nama', 'email', 'bidang', 'alamat'];
+
+
+}
+?>
