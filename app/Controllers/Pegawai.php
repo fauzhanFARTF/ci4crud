b9437f4d4cc106634a0c667224fdc8a71dc3ad99
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-08 19:58:58 +0700

    Modify pegawai_view.php - Add CDN Bootstrap 5.2

--- app/Controllers/Pegawai.php
+++ app/Controllers/Pegawai.php
@@ -3,7 +3,11 @@
 namespace App\Controllers;
 
 class Pegawai extends BaseController
-{
+{   
+    function __construct(){
+        this->model = new \App\Models\ModelPegawai();
+    }
+    
     public function index()
     {
         return view('pegawai_view');
