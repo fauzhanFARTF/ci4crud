d4ea9319e743d6275cb8dcce6fca406725ee5324
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-09 08:14:15 +0700

    Modify Pegawai.php - Create public function simpan example

--- app/Controllers/Pegawai.php
+++ app/Controllers/Pegawai.php
@@ -7,7 +7,9 @@ class Pegawai extends BaseController
     function __construct(){
         $this->model = new \App\Models\ModelPegawai();
     }
-    
+    public function simpan(){
+        return "Saya dari fungsi simpan";
+    }
     public function index()
     {
         return view('pegawai_view');
