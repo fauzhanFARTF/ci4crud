ee914fa412e26480f46d113cc38f5f913b3f7bb5
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-09 08:04:37 +0700

    Modify pegawai_view.php - Add jquery and first example alert

--- app/Views/pegawai_view.php
+++ app/Views/pegawai_view.php
@@ -71,7 +71,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                                <button type="button" class="btn btn-primary">Simpan</button>
+                                <button type="button" class="btn btn-primary" id="tombolSimpan">Simpan</button>
                             </div>
                         </div>
                     </div>
@@ -105,5 +105,11 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script>
+        $('#tombolSimpan').on('click',function(){
+            alert('Saya diklik');
+        })
+    </script>
 </body>
 </html>
