810d5c49bc87efc548ce2a10fecdf2698148ac31
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-09 07:09:44 +0700

    Correction Pegawai.php

--- app/Controllers/Pegawai.php
+++ app/Controllers/Pegawai.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 class Pegawai extends BaseController
 {   
     function __construct(){
-        this->model = new \App\Models\ModelPegawai();
+        $this->model = new \App\Models\ModelPegawai();
     }
     
     public function index()
