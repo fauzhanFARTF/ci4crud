1b90c1e4e1cbb58c57bfbc33b5171d56ef1d276b
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-09 08:10:59 +0700

    Modify pegawai_view.php - Modify Jquery and add Ajax

--- app/Views/pegawai_view.php
+++ app/Views/pegawai_view.php
@@ -108,7 +108,17 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script>
         $('#tombolSimpan').on('click',function(){
-            alert('Saya diklik');
+            var $nama = $('#inputNama').val();
+            var $email = $('#inputEmail').val();
+            var $bidang = $('#inputBidang').val();
+            var $alamat = $('#inputAlamat').val();
+            $.ajax({
+                url: "<?php echo site_url("pegawai/simpan")?>",
+                type: "POST",
+                success: function(hasil){
+                    alert(hasil)
+                }
+            })
         })
     </script>
 </body>
