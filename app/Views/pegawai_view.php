a03ef83609e57fd17c045c19cf551721479f62ca
fauzhanFARTF <fauzan1812@gmail.com>
2023-06-10 11:03:20 +0700

    Create a form clear function on save

--- app/Views/pegawai_view.php
+++ app/Views/pegawai_view.php
@@ -37,7 +37,7 @@
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h1 class="modal-title fs-5" id="exampleModalLabel">Form Pegawai</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="btn-close tombol-tutup" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <div class="alert alert-danger error" role="alert" style="display: none"></div>
@@ -73,7 +73,7 @@
                                 </div>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                                <button type="button" class="btn btn-secondary tombol-tutup" data-bs-dismiss="modal">Tutup</button>
                                 <button type="button" class="btn btn-primary" id="tombolSimpan">Simpan</button>
                             </div>
                         </div>
@@ -123,6 +123,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script>
+        function bersihkan(){
+            $('#inputNama').val('');
+            $('#inputEmail').val('');
+            $('#inputAlamat').val('');
+        }
+
+        $('.tombol-tutup').on('click', function(){
+            if($('.sukses').is(":visible")){
+                window.location.href = "<?php echo current_url() . "?" . $_SERVER['QUERY_STRING'] ?>";
+            }
+            $('.alert').hide();
+            bersihkan();
+        })
         $('#tombolSimpan').on('click',function(){
             var $nama = $('#inputNama').val();
             var $email = $('#inputEmail').val();
@@ -149,7 +162,8 @@
                         $('.sukses').html($obj.sukses);
                     }
                 }
-            })
+            });
+            bersihkan();
         })
     </script>
 </body>
